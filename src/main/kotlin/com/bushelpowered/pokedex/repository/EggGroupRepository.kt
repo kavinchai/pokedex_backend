2c207640f7117ed9662a3cd59d8ceb87c56a4eae
Kavin Chaisawangwong <kavinchai00@gmail.com>
2022-06-30 15:02:47 -0500

    Initialized pokemon egg group repositories

---
+++ src/main/kotlin/com/bushelpowered/pokedex/repository/EggGroupRepository.kt
@@ -0,0 +1,6 @@
+package com.bushelpowered.pokedex.repository
+
+import com.bushelpowered.pokedex.entity.EggGroup
+import org.springframework.data.repository.CrudRepository
+
+interface EggGroupRepository:CrudRepository<EggGroup, Int>
---
+++ src/main/kotlin/com/bushelpowered/pokedex/repository/PokemonEggGroupRepository.kt
@@ -0,0 +1,6 @@
+package com.bushelpowered.pokedex.repository
+
+import com.bushelpowered.pokedex.entity.PokemonEggGroup
+import org.springframework.data.repository.CrudRepository
+
+interface PokemonEggGroupRepository:CrudRepository<PokemonEggGroup, Int>
