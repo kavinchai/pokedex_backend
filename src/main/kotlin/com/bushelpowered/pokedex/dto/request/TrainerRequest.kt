2d8b9443b49ab9bbea43877b5ce4444c0e95ee3b
Kavin Chaisawangwong <kavinchai00@gmail.com>
2022-07-11 13:33:08 -0500

    Added valiktor validation for trainerrequest DTO

--- src/main/kotlin/com/bushelpowered/pokedex/dto/request/TrainerRequest.kt
+++ src/main/kotlin/com/bushelpowered/pokedex/dto/request/TrainerRequest.kt
@@ -1,9 +1,24 @@
 package com.bushelpowered.pokedex.dto.request
 
+import org.valiktor.functions.isEmail
+import org.valiktor.functions.isNotBlank
+import org.valiktor.functions.isPositive
+import org.valiktor.validate
+
 data class TrainerRequest(
     val id: Int,
     val username: String,
     val firstname: String,
     val lastname: String,
     val email: String
-)
+){
+    init{
+        validate(this){
+            validate(TrainerRequest::id).isPositive()
+            validate(TrainerRequest::username).isNotBlank()
+            validate(TrainerRequest::firstname).isNotBlank()
+            validate(TrainerRequest::lastname).isNotBlank()
+            validate(TrainerRequest::email).isNotBlank().isEmail()
+        }
+    }
+}
