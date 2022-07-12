eeb7857755f047e578dbf5f4960d10865ebd8696
Kavin Chaisawangwong <kavinchai00@gmail.com>
2022-07-12 13:59:48 -0500

    Authenticated trainers now displays firstnames of captured pokemon

--- src/main/kotlin/com/bushelpowered/pokedex/dto/response/LoginTrainerResponse.kt
+++ src/main/kotlin/com/bushelpowered/pokedex/dto/response/LoginTrainerResponse.kt
@@ -1,12 +1,10 @@
 package com.bushelpowered.pokedex.dto.response
 
-import com.bushelpowered.pokedex.model.Pokemon
-
 data class LoginTrainerResponse(
     val id: Int,
     val username: String,
     val firstname: String,
     val lastname: String,
     val email: String,
-    val capturedPokemon: List<Pokemon>
+    val capturedPokemon: List<String>
 )
--- src/main/kotlin/com/bushelpowered/pokedex/utils/TrainerToLoginResponse.kt
+++ src/main/kotlin/com/bushelpowered/pokedex/utils/TrainerToLoginResponse.kt
@@ -4,12 +4,13 @@ import com.bushelpowered.pokedex.dto.response.LoginTrainerResponse
 import com.bushelpowered.pokedex.model.Trainer
 
 fun Trainer.toLoginResponse(): LoginTrainerResponse{
+    val pokemonNameList =  this.capturedPokemon.map{it.name}
     return LoginTrainerResponse(
         id = this.id,
         username = this.username,
         firstname = this.firstname,
         lastname = this.lastname,
         email = this.email,
-        capturedPokemon = this.capturedPokemon
+        capturedPokemon = pokemonNameList
     )
 }
